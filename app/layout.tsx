4496e567cdf2c5eaa604711149abdf4fd74acae5
Brown Sugar Bakery <gifting@brownsugarbakerychicago.com>
2026-01-15 21:37:14 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
       >
         <GiftProvider>
           <Layout>
-            {children}
+        {children}
           </Layout>
         </GiftProvider>
       </body>
